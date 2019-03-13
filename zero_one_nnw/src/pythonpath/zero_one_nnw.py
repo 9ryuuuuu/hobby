4916d9e517cc004dd88cbcf84b5931d08bf499e2
Akira Kuriyama <ruiakirab@gmail.com>
2019-03-14 00:21:46 +0900

    nnw backpropagation途中

--- zero_one_nnw/src/pythonpath/zero_one_nnw.py
+++ zero_one_nnw/src/pythonpath/zero_one_nnw.py
@@ -16,6 +16,7 @@ class ZeroOneNNW:
 
     def __init__(self):
         """コンストラクタ。"""
+
         # 入力層の入力ユニット
         self.layer_1_in = np.zeros(self.unit_num_layer_1)
         # 入力層の出力ユニット
@@ -71,13 +72,13 @@ class ZeroOneNNW:
             for i, t_data, t_label in enumerate(zip(train_data, train_label)):
                 self.__forward(t_data, t_label, cost, i)
                 self.__backpropagation(
-                    w_1_gradient, w_2_gradient, b_1_gradient, b_2_gradient)
+                    t_label, w_1_gradient, w_2_gradient, b_1_gradient, b_2_gradient)
             self.__update_parameter(
                 eta, w_1_gradient, w_2_gradient, b_1_gradient, b_2_gradient)
-            print(f'epoc: {e} cost: {self.__get_cost(cost)}')
+            print(f'epoc: {e} cost: {self.__get_total_cost(cost)}')
 
     def __forward(self, t_data: List[int], t_label: List[int],
-                  cost: np.ndarray[float], data_num: int) -> None:
+                  cost: np.ndarray, data_num: int) -> None:
         """Forwardの計算を行う。
 
         データ毎のコストを算出する。
@@ -89,16 +90,26 @@ class ZeroOneNNW:
             cost (np.ndarray):
             data_num (int): データ番号
         """
-        # 各レイヤーの値を計算
-        # 入力データのコスト計算
-        pass
+        self.__initialize_layer()
 
-    def __backpropagation(self, w_1_gradient: np.ndarry,
+        # 各レイヤーの値を計算
+        self.layer_1_in = np.array(t_data)
+        self.layer_1_out = self.layer_1_in
+        self.layer_2_in = np.dot(self.w_1, self.layer_1_out) + self.b_1
+        self.layer_2_out = self.__sigmoid(self.layer_2_in)
+        self.layer_3_in = np.dot(self.w_2, self.layer_3_in) + self.b_2
+        self.layer_3_out = self.__sigmoid(self.layer_3_in)
+
+        # 入力データのコスト計算
+        cost[data_num] = ((self.layer_3_out - t_label)**2).sum()
+
+    def __backpropagation(self, t_label: List[int], w_1_gradient: np.ndarry,
                           w_2_gradient: np.ndarry, b_1_gradient: np.ndarry,
                           b_2_gradient: np.ndarry) -> None:
         """重みとバイアスの微分を求める。
 
         Args:
+            t_label (List[int]): 学習ラベル1件
             w_1_gradient (np.ndarry): 中間層への重みの勾配
             w_2_gradient (np.ndarry): 中間層へのバイアスの勾配
             b_1_gradient (np.ndarry): 出力層への重みの勾配
@@ -121,9 +132,11 @@ class ZeroOneNNW:
         # 中間層へのバイアスの微分
         error_b_1 = np.zeros((self.unit_num_layer_2))
 
-        # 微分を計算
+        # 各レイヤーの微分を計算
+        error_3_out = self.layer_3_out - t_label
+        error_3_in = error_3_out * self.__derivative_sigmoid(self.layer_3_in)
+
         # w_1_gradient, w_2_gradient, b を算出。アイテムごとに、重みとバイアスの微分を足し合わせる
-        pass
 
     def __update_parameter(self, eta: float, w_1_gradient: np.ndarry,
                            w_2_gradient: np.ndarry,
@@ -162,3 +175,18 @@ class ZeroOneNNW:
             List[str]: 予測結果の配列
         """
         pass
+
+    def __initialize_layer(self):
+        """各レイヤーのユニットの値を初期化(ゼロクリア)する。"""
+        self.layer_1_in *= 0
+        self.layer_1_out *= 0
+        self.layer_2_in *= 0
+        self.layer_2_out *= 0
+        self.layer_3_in *= 0
+        self.layer_3_out *= 0
+
+    def __sigmoid(x: np.array) -> np.darray:
+        return 1 / (1 + np.exp(-x))
+
+    def __derivative_sigmoid(self, x: np.ndarray) -> np.ndarray:
+        return self.__sigmoid(x) * (1 - self.__sigmoid(x))
